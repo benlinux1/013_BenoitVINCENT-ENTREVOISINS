64858859447ed31ea268162ddc6ff498b7908856
Ben_Linux <benoit.vincent2707@gmail.com>
2022-03-28 17:33:26 +0200

    Edit ProfileActivity Layout

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
@@ -1,13 +1,20 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 
 import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.events.DeleteNeighbourEvent;
+
+import org.greenrobot.eventbus.EventBus;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,6 +44,12 @@ public class ListNeighbourActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
         mTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
+    }
+
+    @OnClick(R.id.tabItem2)
+    public void goToFavorites() {
+        Intent FavoritesActivityIntent = new Intent(ListNeighbourActivity.this, FavoritesActivity.class);
+        startActivity(FavoritesActivityIntent);
     }
 
     @OnClick(R.id.add_neighbour)
---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.openclassrooms.entrevoisins.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
